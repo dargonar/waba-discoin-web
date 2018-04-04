6f024b92bbc6d34b329c8aa2a23c73c8df7e038e
Marcos Gutierrez <gmarcos87@gmail.com>
2018-04-04 09:55:30 -0300

    Add balance widgets to dashboard

--- src/containers/dashboard.js
+++ src/containers/dashboard.js
@@ -1,14 +1,72 @@
 import React, { Component } from 'react';
 import LayoutContentWrapper from '../components/utility/layoutWrapper';
 import LayoutContent from '../components/utility/layoutContent';
+import IsoWidgetsWrapper from '../components/utility/widgets-wrapper';
+import { Row, Col } from 'antd';
+import basicStyle from '../config/basicStyle';
+
+import BalanceSticker from '../components/balance-sticker/balance-sticker'
 
 export default class extends Component {
   render() {
+    const { rowStyle, colStyle } = basicStyle;
     return (
       <LayoutContentWrapper style={{ height: '100vh' }}>
         <LayoutContent>
-        </LayoutContent>
-      </LayoutContentWrapper>
-    );
-  }
+          <Row style={rowStyle} gutter={0} justify="start">
+            <Col md={6} sm={12} xs={24} style={colStyle}>
+              <IsoWidgetsWrapper>
+                <BalanceSticker
+                  number="53,155"
+                  text="Discoin Balance"
+                  coin="DSC"
+                  fontColor="#1C222C"
+                  bgColor="#f5f5f5"/>
+              </IsoWidgetsWrapper>
+            </Col>
+
+            <Col md={6} sm={12} xs={24} style={colStyle}>
+              <IsoWidgetsWrapper>
+                <BalanceSticker
+                  number="90,000"
+                  text="Initial Credit"
+                  coin="DSC"
+                  fontColor="#1C222C"
+                  bgColor="#f5f5f5"/>
+              </IsoWidgetsWrapper>
+            </Col>
+
+            <Col md={6} sm={12} xs={24} style={colStyle}>
+              <IsoWidgetsWrapper>
+                <BalanceSticker
+                  number={59.6}
+                  text="Accepted / Received ratio"
+                  scale={[
+                    {value:10, color:"red"},
+                    {value:50, color:"yellow"},
+                      {value:100, color:"green"}
+                    ]}
+                    percentage={true}
+                    fontColor="#1C222C"
+                    bgColor="#f5f5f5"/>
+              </IsoWidgetsWrapper>
+            </Col>
+
+            <Col md={6} sm={12} xs={24} style={colStyle}>
+              <IsoWidgetsWrapper>
+                <BalanceSticker
+                  number={59.6}
+                  percentage={true}
+                  text="Reward & Refund"
+                  subtext="24% @ category"
+                  fontColor="#1C222C"
+                    bgColor="#f5f5f5"/>
+              </IsoWidgetsWrapper>
+            </Col>
+
+          </Row>
+      </LayoutContent>
+    </LayoutContentWrapper>
+  );
+}
 }
