f3e97d84e53c7fccf4420b289bddab1786e22ce1
Marcos Gutierrez <gmarcos87@gmail.com>
2018-04-26 10:42:37 -0300

    Remove comments

--- src/redux/business/saga.js
+++ src/redux/business/saga.js
@@ -4,10 +4,21 @@ import fakeApi from './fakeData';
 
 import { getPath, apiCall } from '../../httpService';
 
+
+function* signTx(action) {
+    yield takeEvery('signTX', function*(action) {
+        const url = getPath('URL/SIGN_TX',{ ...action.payload })
+        const fetchData = apiCall(url, 'POST', action.payload.toSign)
+        const { data, ex } = yield call(fetchData);
+        if (data)
+            yield put({ type: action.payload.onSuccess, payload: data.businesses });
+           else
+            yield put({ type: action.payload.onFail, ex });
+    })
+};
+
 function* getBusinesses(action) {
     yield takeLatest(actions.FETCH_CONFIGURATION_BUSINESSES, function*(action) {
-        // const url = getBusinessPath + 'list/' + action.payload.balance + '/' + action.payload.from + '/' + action.payload.limit;
-        //const url = getBusinessPath + 'list/' + action.payload.from + '/' + action.payload.limit;
         const url = getPath('URL/GET_BUSINESSES',{ ...action.payload })
         const fetchData = apiCall(url)
         const { data, ex } = yield call(fetchData);
@@ -27,9 +38,14 @@ function* setOverdraft(action) {
         };
         const fetchData = apiCall(url, 'POST', body, console.log)
         const { data, ex } = yield call(fetchData);
-        if (data && typeof data.error === 'undefined' ) {
-            yield put({ type: actions.BUSINESS_SET_OVERDRAFT_SUCCESS, payload: {id : action.payload.account_id }});
-        }
+
+        if (data && typeof data.error === 'undefined' ) 
+            //Dispatch other action -> signTX
+            yield put({ type: 'signTX', payload: {
+                toSign: data,
+                onSuccess: actions.BUSINESS_SET_OVERDRAFT_SUCCESS,
+                onFail: actions.BUSINESS_SET_OVERDRAFT_FAILD
+            }});
         else
             yield put({ type: actions.BUSINESS_SET_OVERDRAFT_FAILD, payload: ex });
     })
@@ -51,7 +67,8 @@ export default function* rootSaga() {
     yield all([
       fork(getBusinesses),
       fork(setOverdraft),
-      fork(reloadBusiness)
+      fork(reloadBusiness),
+      fork(signTx)
     ]);
   }
   
