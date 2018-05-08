9322ae5b553632a2625e9d5d2f769535b14f93a7
Marcos Gutierrez <gmarcos87@gmail.com>
2018-05-07 23:09:07 -0300

    Remove redux logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,14 +3,13 @@ import createHistory from 'history/createBrowserHistory';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import thunk from 'redux-thunk';
 import createSagaMiddleware from 'redux-saga';
-import logger from 'redux-logger'
 import reducers from './reducers';
 import rootSaga from './sagas';
 
 const history = createHistory();
 const sagaMiddleware = createSagaMiddleware();
 const routeMiddleware = routerMiddleware(history);
-const middlewares = [thunk, sagaMiddleware, routeMiddleware, logger];
+const middlewares = [thunk, sagaMiddleware, routeMiddleware];
 
 const composeEnhancers =
   typeof window === 'object' &&
