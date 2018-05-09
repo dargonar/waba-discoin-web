c0f0ed5776798919bb364225ea7c86a1f8ac1365
Marcos Gutierrez <gmarcos87@gmail.com>
2018-05-09 17:45:37 -0300

    Get bussiness id from store

--- src/redux/api/actions.js
+++ src/redux/api/actions.js
@@ -4,14 +4,14 @@ const actions = {
     GET_PROFILE_FAILD: 'GET_PROFILE_FAILD',
     CLEAR_MSG: 'CLEAR_MSG',
 
-    fetchProfile: (account_id) => (dispatch) => 
+    fetchProfile: (account_id) => (dispatch, getState) => {
         dispatch({
             type: actions.GET_PROFILE,
             payload: {
-                account_id: account_id
+                account_id: account_id || getState().Auth.get('businessId')
             }
-        }),
-
+        })
+    },
     cleanMsg: () => (dispatch) => 
         dispatch({
             type: actions.CLEAR_MSG
