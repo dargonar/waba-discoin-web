753d3995a4e1ca5d8a2258364199b974170ab462
Marcos Gutierrez <gmarcos87@gmail.com>
2018-05-10 14:32:09 -0300

    Bind discount and rewards page to redux

--- src/containers/Page/discountAndRewards.js
+++ src/containers/Page/discountAndRewards.js
@@ -8,15 +8,21 @@ import basicStyle from '../../config/basicStyle';
 import Box from '../../components/utility/box';
 import ContentHolder from '../../components/utility/contentHolder';
 import Input from '../../components/uielements/input';
+import Button from '../../components/uielements/button';
+import AlertBox from '../../components/MessageBox';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import actions from '../../redux/api/actions';
+import MessageBox from '../../components/MessageBox';
 
 class DiscountsAndRewards extends Component {
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {};
     this.renderContent = this.renderContent.bind(this);
+    this.updateRefounds = this.updateRefounds.bind(this);
+    this.submit = this.submit.bind(this);
   }
 
   componentWillMount() {
@@ -25,39 +31,21 @@ class DiscountsAndRewards extends Component {
     }
   }
 
+  updateRefounds(e, key) {
+    const value = e.target.value
+    let discounts = this.state.discounts || this.props.api.business.discount_schedule;
+    if (discounts[key]) { discounts[key] = { ...discounts[key], discount: value } }
+    this.setState({ discounts: discounts });
+  }
+
+  submit() {
+    if (typeof this.state.discounts !== 'undefined') {
+      this.props.updateSchedule(this.state.discounts)  
+    }
+  }
+
   renderContent() {
     const { rowStyle, colStyle } = basicStyle;
-    const discounts = [
-      {
-        label: 'Monday',
-        value: 30,
-        avg: 28.6
-      },{
-        label: 'Tuesday',
-        value: 30,
-        avg: 28.6
-      },{
-        label: 'Wednesday',
-        value: 30,
-        avg: 28.6
-      },{
-        label: 'Thursday',
-        value: 30,
-        avg: 28.6
-      },{
-        label: 'Friday',
-        value: 30,
-        avg: 28.6
-      },{
-        label: 'Saturday',
-        value: 30,
-        avg: 28.6
-      },{
-        label: 'Sunday',
-        value: 30,
-        avg: 28.6
-      }
-    ]
     
     const inputStyle = {
       fontSize:'24px'
@@ -68,6 +56,7 @@ class DiscountsAndRewards extends Component {
     }
 
     return (
+    <div>
       <Row style={rowStyle} gutter={16} justify="start">
         {this.props.api.business.discount_schedule.map((data, key) =>
           (<Col md={6} sm={12} xs={24} style={colStyle} key={key}>
@@ -75,19 +64,31 @@ class DiscountsAndRewards extends Component {
               <span style={{textTransform:'capitalize'}}>{data.date}</span>
             )}>
               <ContentHolder>
-                <Input type='number' defaultValue={data.discount} style={inputStyle}/>
+                <Input type='number' defaultValue={data.discount} style={inputStyle} onChange={(e) => this.updateRefounds(e, key)} />
                 {(data.avg)? (<span style={avgStyle}>Avg. {Number(data.avg).toLocaleString()} %</span>): false }
               </ContentHolder>
             </Box>
           </Col>)
         )}
       </Row>
+      <Button 
+        type="primary"
+        style={{marginLeft: 'auto', marginRight: '16px'}}
+        onClick={this.submit}
+        loading={this.props.api.actionLoading}>
+          Apply changes
+      </Button>
+    </div>
     );
   }
 
   render() {
     return (
       <LayoutContentWrapper>
+        <MessageBox
+          clean={this.props.cleanMsg}
+          msg={this.props.api.msg}
+          error={this.props.api.error !== false} /> 
         <PageHeader>
           Discounts and Rewards
         </PageHeader>
@@ -108,6 +109,7 @@ const mapDispatchToProps = (dispatch) => ({
   cleanMsg: bindActionCreators(actions.cleanMsg, dispatch),
   fetchProfile: bindActionCreators(actions.fetchProfile, dispatch),
   fetchConfiguration: bindActionCreators(actions.fetchConfiguration, dispatch),
+  updateSchedule: bindActionCreators(actions.updateSchedule, dispatch)
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(DiscountsAndRewards);
