a30e2be07fff712904e8b2b7ec752e5a6d6ff6f3
Marcos Gutierrez <gmarcos87@gmail.com>
2018-05-10 08:26:08 -0300

    Dispaly api data in discount and rewards

--- src/containers/Page/discountAndRewards.js
+++ src/containers/Page/discountAndRewards.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import LayoutContentWrapper from '../../components/utility/layoutWrapper';
 import LayoutContent from '../../components/utility/layoutContent';
+import PageHeader from '../../components/utility/pageHeader';
+import PageLoading from '../../components/pageLoading';
 import { Row, Col } from 'antd';
 import basicStyle from '../../config/basicStyle';
 import Box from '../../components/utility/box';
 import ContentHolder from '../../components/utility/contentHolder';
 import Input from '../../components/uielements/input';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import actions from '../../redux/api/actions';
+
 class DiscountsAndRewards extends Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.renderContent = this.renderContent.bind(this);
+  }
+
+  componentWillMount() {
+    if (typeof this.props.api.business === 'null') {
+      this.props.fetchProfile();
+    }
+  }
+
+  renderContent() {
     const { rowStyle, colStyle } = basicStyle;
     const discounts = [
       {
@@ -49,23 +66,48 @@ class DiscountsAndRewards extends Component {
       display: 'block',
       paddingTop: '15px'
     }
+
     return (
-      <LayoutContentWrapper>
-        <Row style={rowStyle} gutter={16} justify="start">
-          {discounts.map(data =>
-            (<Col md={6} sm={12} xs={24} style={colStyle}>
-              <Box title={data.label} >
+      <Row style={rowStyle} gutter={16} justify="start">
+        {this.props.api.business.discount_schedule.map((data, key) =>
+          (<Col md={6} sm={12} xs={24} style={colStyle} key={key}>
+            <Box title={(
+              <span style={{textTransform:'capitalize'}}>{data.date}</span>
+            )}>
               <ContentHolder>
-                <Input value={data.value} style={inputStyle}/>
-                <span style={avgStyle}>Avg. {Number(data.avg).toLocaleString()} %</span>
+                <Input type='number' defaultValue={data.discount} style={inputStyle}/>
+                {(data.avg)? (<span style={avgStyle}>Avg. {Number(data.avg).toLocaleString()} %</span>): false }
               </ContentHolder>
             </Box>
-            </Col>)
-          )}
-        </Row>
+          </Col>)
+        )}
+      </Row>
+    );
+  }
+
+  render() {
+    return (
+      <LayoutContentWrapper>
+        <PageHeader>
+          Discounts and Rewards
+        </PageHeader>
+        { (
+            this.props.api.loading !== false &&
+            typeof this.props.api.configuration === 'null'
+          )? (<PageLoading/>): this.renderContent() }     
       </LayoutContentWrapper>
     );
   }
 }
 
-export default DiscountsAndRewards;
+const mapStateToProps = (state) =>  ({
+  api: state.Api
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  cleanMsg: bindActionCreators(actions.cleanMsg, dispatch),
+  fetchProfile: bindActionCreators(actions.fetchProfile, dispatch),
+  fetchConfiguration: bindActionCreators(actions.fetchConfiguration, dispatch),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(DiscountsAndRewards);
