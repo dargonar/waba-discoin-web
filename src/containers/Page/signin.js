ad9ad5efb6f83a1bebb23ebfd1dc1577b82e13b6
Marcos Gutierrez <gmarcos87@gmail.com>
2018-05-23 00:08:37 -0300

    Add registerbox to signin page

--- src/containers/Page/signin.js
+++ src/containers/Page/signin.js
@@ -5,16 +5,26 @@ import Input from '../../components/uielements/input';
 import Checkbox from '../../components/uielements/checkbox';
 import Button from '../../components/uielements/button';
 import authAction from '../../redux/auth/actions';
+import apiAction from '../../redux/api/actions';
 import IntlMessages from '../../components/utility/intlMessages';
 import SignInStyleWrapper from './signin.style';
 
+import RegisterBox from './components/register';
+import { bindActionCreators } from 'redux';
 const { login } = authAction;
 
 class SignIn extends Component {
-  state = {
-    redirectToReferrer: false,
-    account: null,
-  };
+  constructor(props) {
+    super(props)
+    this.state = {
+      redirectToReferrer: false,
+      account: null,
+      register: false,
+    };
+    this.registerAccount = this.registerAccount.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
+  }
+
   componentWillReceiveProps(nextProps) {
     if (
       this.props.isLoggedIn !== nextProps.isLoggedIn &&
@@ -23,6 +33,11 @@ class SignIn extends Component {
       this.setState({ redirectToReferrer: true });
     }
   }
+  
+  componentWillMount() {
+    this.props.getCategories();
+  }
+
   handleLogin = () => {
     const { login } = this.props;
     login({account: this.state.account});
@@ -37,6 +52,13 @@ class SignIn extends Component {
     }
     return (
       <SignInStyleWrapper className="isoSignInPage">
+        <RegisterBox 
+          visible={this.state.register}
+          cancel={()=>{this.setState({register: false})}}
+          submit={this.props.registerAccount}
+          loading={this.props.loading}
+          error={this.props.error}
+        />
         <div className="isoLoginContentWrapper">
           <div className="isoLoginContent">
             <div className="isoLogoWrapper">
@@ -62,6 +84,12 @@ class SignIn extends Component {
                   <IntlMessages id="page.signInButton" />
                 </Button>
               </div>
+
+              <div className="isoInputWrapper isoOtherLogin">
+                <Button type="primary" onClick={()=>{this.setState({register: true})}}>
+                    <IntlMessages id="page.signUpButton" />
+                </Button>
+              </div>
             </div>
           </div>
         </div>
@@ -70,9 +98,12 @@ class SignIn extends Component {
   }
 }
 
-export default connect(
-  state => ({
-    isLoggedIn: state.Auth.get('idToken') !== null ? true : false,
-  }),
-  { login }
-)(SignIn);
+const mapStateToProps = (state) => ({
+  isLoggedIn: state.Auth.get('idToken') !== null ? true : false
+})
+
+const mapDispatchToProps = (dispatch) => ({
+  login: login,
+  getCategories: bindActionCreators(apiAction.getCategories, dispatch)
+});
+export default connect(mapStateToProps, mapDispatchToProps)(SignIn);
