fcd600c2395a9b8105523060e5295bb1d18e24fe
Marcos Gutierrez <gmarcos87@gmail.com>
2018-06-01 13:17:50 -0300

    Fix loading position

--- src/components/uielements/globalLoading.js
+++ src/components/uielements/globalLoading.js
@@ -8,23 +8,30 @@ const GlobalLoadingWrapper = styled.div`
     top: 0px;
     position: fixed;
     background: rgba(0,0,0,0.7);
-    height: calc(100%);
+    height: 100%;
     width: 100%;
-    padding-top: calc(50% - 78px);
     color: #fff !important;
     text-align: center;
     z-index:10000;
-    span {
-        padding: 5px;
-        display: inline-block;
+    .circle {
+        position: relative;
+        top: 150px;
+        transform-origin: 704.5px 486px 0px;
+        span {
+            padding: 5px;
+            display: inline-block;
+        }
     }
+
   }`;
 
 export const GlobalLoading = ({msg}) => (
     <GlobalLoadingWrapper>
         <div className={"globalLoading"}>
-            <Button shape="circle" loading /><br/>
-            <span>{msg}</span>
+            <div className={"circle"}>
+                <Button shape="circle" loading /><br/>
+                <span>{msg}</span>
+            </div>
         </div>
     </GlobalLoadingWrapper>
 );
