9cc44e32c30778cfbb4fca73b1d562969d66d030
Marcos Gutierrez <gmarcos87@gmail.com>
2018-06-01 13:42:25 -0300

    Remove lazy search
    Related #3

--- src/redux/api/sagas/searchCustomers.js
+++ src/redux/api/sagas/searchCustomers.js
@@ -3,39 +3,20 @@ import { call, put, fork, take, takeEvery } from 'redux-saga/effects';
 import actions from '../actions';
 import { apiCall, getPath } from '../../../httpService';
 
-function* runRequestSuggest(name) {
-  const url = getPath('URL/SEARCH_CUSTOMERS', { name: name, filter: '0' })
-  const fetchData = apiCall(url)
-  
-  const { data, error } = yield call(fetchData);
-  console.log(' --- runRequestSuggest:', name, data);
-  if (data && !error) {
-    yield put({ type: actions.SEARCH_CUSTOMERS_SUCCESS, payload: data });
-  } else {
-    yield put({ type: actions.SEARCH_CUSTOMERS_FAILD, payload: error });
-  }
-}
-
-function createLazily(msec = 1000) {
-  let ongoing;
-  return function* (task, ...args) {
-    if (ongoing && ongoing.isRunning()) {
-      ongoing.cancel();
-    }
-    ongoing = yield fork(function* () {
-      yield call(delay, msec);
-      yield fork(task, ...args);
-    });
-  }
-}
-
 export const searchCustomers = function* () {
-  const lazily = createLazily();
-  while (true) {
     const { payload } = yield take(actions.SEARCH_CUSTOMERS);
-    yield fork(lazily, runRequestSuggest, payload);
+    const url = getPath('URL/SEARCH_CUSTOMERS', { name: payload, filter: '0' })
+    const fetchData = apiCall(url)
+    
+    const { data, error } = yield call(fetchData);
+    console.log(' --- runRequestSuggest:', payload, data);
+    if (data && !error) {
+      yield put({ type: actions.SEARCH_CUSTOMERS_SUCCESS, payload: data });
+    } else {
+      yield put({ type: actions.SEARCH_CUSTOMERS_FAILD, payload: error });
+    }
+  
   }
-}
 
 
 export const searchAllCustomers = function*() {
