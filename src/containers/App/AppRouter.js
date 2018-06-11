0e5761cc8b96765c03f982a91c5c4caabb3851c4
Marcos Gutierrez <gmarcos87@gmail.com>
2018-06-11 12:42:51 -0300

    Eslint approuter

--- src/containers/App/AppRouter.js
+++ src/containers/App/AppRouter.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import asyncComponent from '../../helpers/AsyncFunc';
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import asyncComponent from "../../helpers/AsyncFunc";
 
 class AppRouter extends React.Component {
   render() {
@@ -10,32 +10,38 @@ class AppRouter extends React.Component {
         <Route
           exact
           path={`${url}/`}
-          component={asyncComponent(() => import('../dashboard'))}
+          component={asyncComponent(() => import("../dashboard"))}
         />
         <Route
           exact
           path={`${url}/discount-and-rewards`}
-          component={asyncComponent(() => import('../Page/discountAndRewards'))}
+          component={asyncComponent(() => import("../Page/discountAndRewards"))}
         />
         <Route
           exact
           path={`${url}/refunds`}
-          component={asyncComponent(() => import('../Page/refunds'))}
+          component={asyncComponent(() => import("../Page/refunds"))}
         />
         <Route
           exact
           path={`${url}/sub_accounts`}
-          component={asyncComponent(() => import('../Page/subaccounts/sub_accounts'))}
+          component={asyncComponent(() =>
+            import("../Page/subaccounts/sub_accounts")
+          )}
         />
         <Route
           exact
           path={`${url}/find_account`}
-          component={asyncComponent(() => import('../Page/subaccounts/find_account'))}
+          component={asyncComponent(() =>
+            import("../Page/subaccounts/find_account")
+          )}
         />
         <Route
           exact
           path={`${url}/transactions`}
-          component={asyncComponent(() => import('../Page/transactions/transactions'))}
+          component={asyncComponent(() =>
+            import("../Page/transactions/transactions")
+          )}
         />
       </Switch>
     );
