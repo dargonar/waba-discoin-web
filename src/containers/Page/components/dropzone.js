fb80efba76c5299e428e23fc7eb42f800dd4934a
Marcos Gutierrez <gmarcos87@gmail.com>
2018-06-12 10:51:02 -0300

    Init dorpimage component

---
+++ src/containers/Page/components/dropzone.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Dropzone from "../../../components/uielements/dropzone.js";
+import DropzoneWrapper from "./components/dropzone.style";
+
+export class DropImage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return false;
+  }
+}
