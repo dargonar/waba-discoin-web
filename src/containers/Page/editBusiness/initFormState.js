4f7389431dcd7be1eae43218a35ede819f8f6b8b
Marcos Gutierrez <gmarcos87@gmail.com>
2018-06-14 16:52:38 -0300

    Add initial form state

---
+++ src/containers/Page/editBusiness/initFormState.js
@@ -0,0 +1,63 @@
+export let initFormState = {
+  discount_schedule: [
+    {
+      discount: "0",
+      date: "monday"
+    },
+    {
+      discount: "0",
+      date: "tuesday"
+    },
+    {
+      discount: "0",
+      date: "wednesday"
+    },
+    {
+      discount: "0",
+      date: "thursday"
+    },
+    {
+      discount: "0",
+      date: "friday"
+    },
+    {
+      discount: "0",
+      date: "saturday"
+    },
+    {
+      discount: "0",
+      date: "sunday"
+    }
+  ],
+  refound_schedule: [
+    {
+      refound: "0",
+      date: "monday"
+    },
+    {
+      refound: "0",
+      date: "tuesday"
+    },
+    {
+      refound: "0",
+      date: "wednesday"
+    },
+    {
+      refound: "0",
+      date: "thursday"
+    },
+    {
+      refound: "0",
+      date: "friday"
+    },
+    {
+      refound: "0",
+      date: "saturday"
+    },
+    {
+      refound: "0",
+      date: "sunday"
+    }
+  ],
+  location: {}
+};
