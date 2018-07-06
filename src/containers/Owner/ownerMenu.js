1fe649e17a01961434f2b5124d1ffc821bd67e46
Marcos Gutierrez <gmarcos87@gmail.com>
2018-07-06 19:15:27 -0300

    Add owner menu icons

--- src/containers/Owner/ownerMenu.js
+++ src/containers/Owner/ownerMenu.js
@@ -2,26 +2,31 @@ export default [
   {
     name: "kpis",
     url: "owner/kpis",
+    icon: "ion-android-desktop",
     intlMessage: "sidebar.kpis"
   },
   {
     name: "parameters",
     url: "owner/parameters",
+    icon: "ion-gear-a",
     intlMessage: "sidebar.parameters"
   },
   {
     name: "categories",
     url: "owner/categories",
+    icon: "ion-filing",
     intlMessage: "sidebar.categories"
   },
   {
     name: "store-list",
     url: "owner/store/list",
+    icon: "ion-android-list",
     intlMessage: "sidebar.list"
   },
   {
     name: "store-create",
     url: "owner/store/create",
+    icon: "ion-android-add",
     intlMessage: "sidebar.createStore"
   }
 ];
