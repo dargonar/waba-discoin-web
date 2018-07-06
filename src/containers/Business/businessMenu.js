6174add41531b6b838060791a6628eb021d3a2b4
Marcos Gutierrez <gmarcos87@gmail.com>
2018-07-06 18:24:14 -0300

    Add icons

--- src/containers/Business/businessMenu.js
+++ src/containers/Business/businessMenu.js
@@ -2,31 +2,37 @@ export default [
   {
     name: "home",
     url: "business/home",
+    icon: "ion-android-apps",
     intlMessage: "sidebar.home"
   },
   {
     name: "refunds",
     url: "business/refounds",
+    icon: "ion-calculator",
     intlMessage: "sidebar.refound"
   },
   {
     name: "transactions",
     url: "business/transactions",
+    icon: "ion-filing",
     intlMessage: "sidebar.transactions"
   },
   {
     name: "subAccounts",
     url: "business/sub_accounts",
+    icon: "ion-android-people",
     intlMessage: "sidebar.subAccounts"
   },
   {
     name: "discountRewards",
     ulr: "business/discount-and-rewards",
+    icon: "ion-settings",
     intlMessage: "sidebar.discountRewards"
   },
   {
     name: "profile",
     url: "business/profile",
+    icon: "ion-android-person",
     intlMessage: "sidebar.profile"
   }
 ];
