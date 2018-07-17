a1007eb263ec2653198af54d30278f358e2ac19b
Marcos Gutierrez <gmarcos87@gmail.com>
2018-07-17 11:31:59 -0300

    Basic state manage in category modal

--- src/containers/Owner/businessCategories/components/categoryModal.js
+++ src/containers/Owner/businessCategories/components/categoryModal.js
@@ -7,41 +7,98 @@ import Select, { SelectOption } from "../../../../components/uielements/select";
 export default class CategoryModal extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      showDiscount: true,
+      form: {
+        name: undefined,
+        description: undefined,
+        discount: undefined,
+        parent_id: 0
+      }
+    };
     this.submit = this.submit.bind(this);
+    this.changeForm = this.changeForm.bind(this);
   }
 
   submit() {
-    this.props.onOk(this.props.form);
+    let formValue = this.state.form;
+    if (formValue.parent_id !== 0) {
+      delete formValue.discount;
+    }
+    this.props.onOk(formValue);
+  }
+
+  componentWillMount() {
+    this.setState({
+      form: this.props.form
+    });
+  }
+
+  changeForm(field, value) {
+    let form = this.state.form;
+    form[field] = value;
+    this.setState({ form });
   }
 
   render() {
     return (
       <Modal
-        title={this.props.edit ? "Edit category" : "Add category"}
+        title={
+          this.props.form.id ? (
+            <IntlMessages id={"Edit Category"} />
+          ) : (
+            <IntlMessages id={"Add Category"} />
+          )
+        }
         visible={this.props.visible}
         onCancel={this.props.onCancel}
         onOk={this.submit}
       >
         <Form>
-          <label>Name</label>
-          <Input defaultValue={this.props.form.name} />
-          <label>Description</label>
-          <Input defaultValue={this.props.form.description} />
-          <label>Parent category</label>
+          <label>
+            <IntlMessages id={"Name"} />
+          </label>
+          <Input
+            defaultValue={this.props.form.name}
+            onChange={e => this.changeForm("name", e.target.value)}
+          />
+          <label>
+            <IntlMessages id={"Description"} />
+          </label>
+          <Input
+            defaultValue={this.props.form.description}
+            onChange={e => this.changeForm("description", e.target.value)}
+          />
+          <label>
+            <IntlMessages id={"Parent Category"} />
+          </label>
           <Select
             style={{ width: "100%" }}
-            defaultValue={this.props.form.parent_id}
+            defaultValue={this.props.form.parent_id || 0}
+            onChange={value => this.changeForm("parent_id", value)}
           >
+            <SelectOption value={0}>
+              <IntlMessages id={"None"} />
+            </SelectOption>
             {this.props.categories.map(category => (
               <SelectOption value={category.id}>{category.name}</SelectOption>
             ))}
           </Select>
-          <label>Discount</label>
-          <Input
-            type="number"
-            defaultValue={Number(this.props.form.discount) || 0}
-          />
+          {this.state.form.parent_id ? (
+            false
+          ) : (
+            <div>
+              <label>
+                <IntlMessages id={"Discount"} />
+              </label>
+              <Input
+                type="number"
+                addonBefore="%"
+                defaultValue={Number(this.props.form.discount) || 0}
+                onChange={e => this.changeForm("discount", e.target.value)}
+              />
+            </div>
+          )}
         </Form>
       </Modal>
     );
