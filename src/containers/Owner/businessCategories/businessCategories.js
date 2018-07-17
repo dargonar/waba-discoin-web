9fe954461e61b5b302fa560a10c93562e3f7af8d
Marcos Gutierrez <gmarcos87@gmail.com>
2018-07-17 09:42:04 -0300

    Bind category edit modal

--- src/containers/Owner/businessCategories/businessCategories.js
+++ src/containers/Owner/businessCategories/businessCategories.js
@@ -3,46 +3,96 @@ import LayoutContentWrapper from "../../../components/utility/layoutWrapper";
 import PageHeader from "../../../components/utility/pageHeader";
 import PageLoading from "../../../components/pageLoading";
 import IntlMessages from "../../../components/utility/intlMessages";
-import MessageBox from "../../../components/MessageBox";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import apiActions from "../../../redux/api/actions";
+import CategoryModal from "./components/categoryModal";
 
 class Categories extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      modal: {
+        visible: false,
+        edit: false,
+        form: {}
+      }
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.submitCategory = this.submitCategory.bind(this);
   }
 
   componentWillMount() {
     this.props.fetchCategories();
   }
 
+  toggleModal() {
+    this.setState({
+      modal: {
+        ...this.state.modal,
+        visible: !this.state.modal.visible
+      }
+    });
+  }
+
+  submitCategory(data) {
+    console.log("CREATE OR UPDATE CATEGORY", data);
+    this.toggleModal();
+  }
+
   renderPage() {
-    return (
-      <ul>
-        {this.props.categories.map(category => (
-          <li>
-            {category.name}
-            <ul>
-              {this.props.subcategories
-                .filter(subcategory => subcategory.parent_id === category.id)
-                .map(subcategory => <li>{subcategory.name}</li>)}
-            </ul>
-          </li>
-        ))}
-      </ul>
+    const printCategoryItem = data => (
+      <span key={data.id} onClick={() => this.showEdit(data)}>
+        {data.name}
+      </span>
     );
+
+    return (
+      <div>
+        <button onClick={this.toggleModal}>Add new</button>
+        {this.state.modal.visible ? (
+          <CategoryModal
+            {...this.state.modal}
+            onCancel={this.toggleModal}
+            onOk={this.submitCategory}
+            categories={this.props.categories}
+          />
+        ) : (
+          false
+        )}
+        <ul>
+          {this.props.categories.map(category => (
+            <li key={category.id}>
+              {printCategoryItem(category)}
+              <ul>
+                {this.props.subcategories
+                  .filter(subcategory => subcategory.parent_id === category.id)
+                  .map(subcategory => (
+                    <li>{printCategoryItem(subcategory)}</li>
+                  ))}
+              </ul>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  showEdit(categotyData) {
+    this.setState({
+      modal: {
+        ...this.state.modal,
+        visible: true,
+        form: {
+          ...categotyData
+        }
+      }
+    });
   }
 
   render() {
     return (
       <LayoutContentWrapper>
-        <MessageBox
-          msg={this.props.msg}
-          error={this.props.error}
-          clean={this.props.removeMsg}
-        />
         <PageHeader>
           <IntlMessages id="sidebar.businessCategories" />
         </PageHeader>
