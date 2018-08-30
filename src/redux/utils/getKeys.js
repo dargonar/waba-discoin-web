71f7211a34d24f7df3ad6d00bb5e742972cb6835
Marcos Gutierrez <gmarcos87@gmail.com>
2018-08-30 12:00:31 -0300

    New way to access encrypted keys.
    This method listens to key requests and user feedback.

---
+++ src/redux/utils/getKeys.js
@@ -0,0 +1,35 @@
+import appActions from "../app/actions";
+import { store } from "../store";
+
+export const getKeys = () => {
+  return new Promise((res, rej) => {
+    // Si está desencriptado envio las keys
+    if (store.getState().Auth.encrypted === false) {
+      res(store.getState().Auth.keys);
+      return;
+    }
+    //Si está encriptado pregunto al usuario
+    const passwordBox = store.getState().App.get("passwordBox");
+    const loading = store.getState().Auth.loading;
+
+    if (!passwordBox && !loading) {
+      // Muestro el box de autorización
+      store.dispatch({ type: appActions.ASK_PASSWORD });
+      // Escucho el store para ver los cambios
+      store.subscribe(() => {
+        //Si el box ya no se muestra más y tampoco hay un loading en auth
+        //significa que o se canceló la acción o terminó el login
+        const actualPasswordBox = store.getState().App.get("passwordBox");
+        const actualLoading = store.getState().Auth.loading;
+        if (!actualPasswordBox && !loading) {
+          //segun lo que ocurrió envio la respuesta
+          if (store.getState().Auth.encrypted === false) {
+            res(store.getState().Auth.keys);
+          } else {
+            rej("encrypted");
+          }
+        }
+      });
+    }
+  });
+};
