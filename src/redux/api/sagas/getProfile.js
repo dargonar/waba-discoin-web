89af5d0176fc79c03969a72123089a53aeb179eb
Marcos Gutierrez <gmarcos87@gmail.com>
2018-08-30 11:57:15 -0300

    Eslint getProfile

--- src/redux/api/sagas/getProfile.js
+++ src/redux/api/sagas/getProfile.js
@@ -1,19 +1,20 @@
-import { call, takeEvery, put } from 'redux-saga/effects';
-import actions from '../actions';
-import { apiCall, getPath } from '../../../httpService';
+import { call, takeEvery, put } from "redux-saga/effects";
+import actions from "../actions";
+import { apiCall, getPath } from "../../../httpService";
 
-export const getProfile = function* () {
-    yield takeEvery(actions.GET_PROFILE, function*(action) {
-        const url = getPath('URL/GET_PROFILE', { id: action.payload.account_id });
-        const fetchData = apiCall(url);
-        
-        const { data, err } = yield call(fetchData)
-        if(data) {
-            yield put({ type: actions.GET_PROFILE_SUCCESS, payload: data })
-            // As they share the same type of response I can update the business status as well.
-            yield put({ type: actions.GET_SCHEDULE_SUCCESS, payload: { discount_schedule: data.business.discount_schedule } })
-        }            
-        else
-            yield put({ type: actions.GET_PROFILE_FAILD, payload: err })
-    });
+export const getProfile = function*() {
+  yield takeEvery(actions.GET_PROFILE, function*(action) {
+    const url = getPath("URL/GET_PROFILE", { id: action.payload.account_id });
+    const fetchData = apiCall(url);
+
+    const { data, err } = yield call(fetchData);
+    if (data) {
+      yield put({ type: actions.GET_PROFILE_SUCCESS, payload: data });
+      // As they share the same type of response I can update the business status as well.
+      yield put({
+        type: actions.GET_SCHEDULE_SUCCESS,
+        payload: { discount_schedule: data.business.discount_schedule }
+      });
+    } else yield put({ type: actions.GET_PROFILE_FAILD, payload: err });
+  });
 };
