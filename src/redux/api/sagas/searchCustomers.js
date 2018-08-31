91dc60b4ae4016f4471ea241bb80004a44c01319
Marcos Gutierrez <gmarcos87@gmail.com>
2018-08-31 11:17:40 -0300

    Remove deprecated code

--- src/redux/api/sagas/searchCustomers.js
+++ src/redux/api/sagas/searchCustomers.js
@@ -1,38 +1,25 @@
-import { delay } from 'redux-saga';
-import { call, put, fork, take, takeEvery } from 'redux-saga/effects';
-import actions from '../actions';
-import { apiCall, getPath } from '../../../httpService';
+import { call, put, takeEvery } from "redux-saga/effects";
+import actions from "../actions";
+import { apiCall, getPath } from "../../../httpService";
 
-  // #   0 = ALL
-  // #   1 = NO_CREDIT && NO_BLACK
-  // #   2 = HAS_CREDIT
-export const searchAccount = function* () {
+// #   0 = ALL
+// #   1 = NO_CREDIT && NO_BLACK
+// #   2 = HAS_CREDIT
+export const searchAccount = function*() {
   yield takeEvery(actions.SEARCH_ACCOUNT, function*(action) {
-    console.log( ' --- saga::searchAccount' , JSON.stringify(action.payload));
-    const url = getPath('URL/SEARCH_CUSTOMERS', { name: action.payload.account_name||'*', filter: action.payload.filter || '0' })
-    const fetchData = apiCall(url)
+    console.log(" --- saga::searchAccount", JSON.stringify(action.payload));
+    const url = getPath("URL/SEARCH_CUSTOMERS", {
+      name: action.payload.account_name || "*",
+      filter: action.payload.filter || "0"
+    });
+    const fetchData = apiCall(url);
 
     const { data, error } = yield call(fetchData);
-    console.log(' --- runRequestSuggest:', action.payload, data, error);
+    console.log(" --- runRequestSuggest:", action.payload, data, error);
     if (data && !data.error && !error) {
       yield put({ type: actions.SEARCH_ACCOUNT_SUCCESS, payload: data });
     } else {
       yield put({ type: actions.SEARCH_ACCOUNT_FAILD, payload: error });
     }
-  })
-}
-
-// export const searchAllCustomers = function*() {
-//   yield takeEvery(actions.SEARCH_ALL_CUSTOMERS, function*() {
-//     const url = getPath('URL/ALL_CUSTOMERS');
-//     const fetchData = apiCall(url)
-//
-//     const { data, error } = yield call(fetchData);
-//     console.log(' --- runRequestSuggest:', data);
-//     if (data && !error) {
-//       yield put({ type: actions.SEARCH_CUSTOMERS_SUCCESS, payload: data });
-//     } else {
-//       yield put({ type: actions.SEARCH_CUSTOMERS_FAILD, payload: error });
-//     }
-//   })
-// }
+  });
+};
