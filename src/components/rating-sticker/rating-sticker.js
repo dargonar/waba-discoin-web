f43e3d28e6b2ff797b4ad69d17f674e4aed218e3
Marcos Gutierrez <gmarcos87@gmail.com>
2018-08-31 11:14:57 -0300

    Remove deprecated code

--- src/components/rating-sticker/rating-sticker.js
+++ src/components/rating-sticker/rating-sticker.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
-import { RatingStickerWidgetWrapper } from './style';
-import { Icon } from 'antd';
-import Review from '../../components/uielements/rate';
+import React, { Component } from "react";
+import { RatingStickerWidgetWrapper } from "./style";
+import { Icon } from "antd";
+import Review from "../../components/uielements/rate";
 
-const StarIcon = ({full}) =>
-  (<Icon type={(full)? 'star': 'star-o'} />);
+const StarIcon = ({ full }) => <Icon type={full ? "star" : "star-o"} />;
 
 export default class extends Component {
-
   render() {
-    const { fontColor, bgColor, width,icon, text, stars, full, rating } = this.props;
+    const { fontColor, bgColor, width, icon, rating } = this.props;
 
     const textColor = {
       color: fontColor
@@ -19,21 +17,11 @@ export default class extends Component {
       width: width
     };
 
-    const makeStars = function(stars,full) {
-
-        return (
-            <div>
-                {
-                    Array(full).fill(1).map((el, i) => (i < stars)? (<StarIcon full={true} key={i} />) : (<StarIcon full={false} key={i} />) )
-                }
-            </div>
-        )
-    }
-
-    // {makeStars(stars,full)}
     return (
-      <RatingStickerWidgetWrapper className="isoRatingStickerWidget" style={widgetStyle}>
-
+      <RatingStickerWidgetWrapper
+        className="isoRatingStickerWidget"
+        style={widgetStyle}
+      >
         <div className="isoContentWrapper">
           <h3 className="isoStatNumber" style={textColor}>
             <Review allowHalf disabled defaultValue={rating.rating} />
