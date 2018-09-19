9ff58295aa27252ebaa5873b9d018815baec5447
Marcos Gutierrez <gmarcos87@gmail.com>
2018-09-19 00:52:04 -0300

    Add select customer modal and list of transactions

---
+++ src/containers/Business/main/components/selectCustomer.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import { Modal, List, Avatar } from "antd";
+import IntlMessages from "../../../../components/utility/intlMessages";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import actions from "../../../../redux/api/actions";
+import HashImg from "../../../../components/hashImage";
+
+class CustomerPicker extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentWillReceiveProps(newProps) {
+    if (this.props.visible !== newProps.visible && newProps.visible === true) {
+      this.props.searchAccount();
+    }
+  }
+
+  render() {
+    return (
+      <Modal
+        visible={this.props.visible}
+        title={
+          <IntlMessages
+            defaultMessage="Select customer"
+            id="businessMain.selectCustomer"
+          />
+        }
+        footer={null}
+        onCancel={this.props.onCancel}
+      >
+        <List
+          dataSource={this.props.customers}
+          renderItem={customer => (
+            <List.Item
+              onClick={() => this.props.onSelect(customer)}
+              style={{ cursor: "pointer" }}
+            >
+              <List.Item.Meta
+                title={customer.name}
+                avatar={<HashImg text={customer.name} size={50} />}
+              />
+            </List.Item>
+          )}
+        />
+      </Modal>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  customers: state.Api.customers || []
+});
+
+const dispatchToProps = dispatch => ({
+  searchAccount: bindActionCreators(actions.searchAccount, dispatch)
+});
+
+export default connect(
+  mapStateToProps,
+  dispatchToProps
+)(CustomerPicker);
---
+++ src/containers/Business/main/components/transactionList.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Transaction from "../../transactions/components/transactionBox";
+
+export class TransactionList extends Component {
+  render() {
+    return (
+      <div>
+        {this.props.txs
+          ? this.props.txs.map(tx => (
+              <Transaction transaction={tx} style={{ margin: "10px 0" }} />
+            ))
+          : false}
+      </div>
+    );
+  }
+}
