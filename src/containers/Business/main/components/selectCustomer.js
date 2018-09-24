78be8d1b453e335b71891f5a740b324dbd3d9010
Marcos Gutierrez <gmarcos87@gmail.com>
2018-09-24 09:38:22 -0300

    Add customer filter in modal

--- src/containers/Business/main/components/selectCustomer.js
+++ src/containers/Business/main/components/selectCustomer.js
@@ -5,11 +5,34 @@ import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import actions from "../../../../redux/api/actions";
 import HashImg from "../../../../components/hashImage";
+import { injectIntl } from "react-intl";
+import { Row, Col, Input } from "antd";
+
+const InputSearch = Input.Search;
 
 class CustomerPicker extends Component {
+  constructor(props) {
+    super(props);
+    this._handleChange = this._handleChange.bind(this);
+    this._handleKeyPress = this._handleKeyPress.bind(this);
+    this.state = { searchValue: undefined };
+  }
+
   componentWillReceiveProps(newProps) {
     if (this.props.visible !== newProps.visible && newProps.visible === true) {
       this.props.searchAccount();
+    }
+  }
+
+  _handleChange(e) {
+    this.setState({ searchValue: e.target.value });
+  }
+
+  _handleKeyPress(e) {
+    let searchValue = this.state.searchValue;
+    console.log(" -- _handleKeyPress:", { value: searchValue });
+    if (e.key === "Enter") {
+      this.props.searchAccount(searchValue);
     }
   }
 
@@ -26,27 +49,50 @@ class CustomerPicker extends Component {
         footer={null}
         onCancel={this.props.onCancel}
       >
-        <List
-          dataSource={this.props.customers}
-          renderItem={customer => (
-            <List.Item
-              onClick={() => this.props.onSelect(customer)}
-              style={{ cursor: "pointer" }}
-            >
-              <List.Item.Meta
-                title={customer.name}
-                avatar={<HashImg text={customer.name} size={50} />}
-              />
-            </List.Item>
-          )}
-        />
+        <Row gutter={16} justify="start">
+          <Col xs={24} style={{ marginBottom: "15px" }}>
+            <InputSearch
+              placeholder={
+                this.props.intl.messages["refund.searchCustomer"] ||
+                "Search Customer"
+              }
+              onKeyPress={this._handleKeyPress}
+              onSearch={() => this.props.searchAccount(this.state.searchValue)}
+              onChange={this._handleChange}
+              enterButton={
+                <IntlMessages
+                  defaultMessage="Force Search"
+                  id="refund.forceSearch"
+                />
+              }
+            />
+          </Col>
+          <Col xs={24}>
+            <List
+              loading={this.props.isLoading}
+              dataSource={this.props.customers}
+              renderItem={customer => (
+                <List.Item
+                  onClick={() => this.props.onSelect(customer)}
+                  style={{ cursor: "pointer" }}
+                >
+                  <List.Item.Meta
+                    title={customer.name}
+                    avatar={<HashImg text={customer.name} size={50} />}
+                  />
+                </List.Item>
+              )}
+            />
+          </Col>
+        </Row>
       </Modal>
     );
   }
 }
 
 const mapStateToProps = state => ({
-  customers: state.Api.customers || []
+  customers: state.Api.customers || [],
+  isLoading: state.Api.actionLoading
 });
 
 const dispatchToProps = dispatch => ({
@@ -56,4 +102,4 @@ const dispatchToProps = dispatch => ({
 export default connect(
   mapStateToProps,
   dispatchToProps
-)(CustomerPicker);
+)(injectIntl(CustomerPicker));
