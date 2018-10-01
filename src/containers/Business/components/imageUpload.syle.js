89dd42218dbe064bc007caf6cc6cebd5e34e0651
Marcos Gutierrez <gmarcos87@gmail.com>
2018-10-01 13:20:40 -0300

    Add new image uploader

---
+++ src/containers/Business/components/imageUpload.syle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default styled.div`
+  .ant-upload-select-picture-card i {
+    font-size: 32px;
+    color: #999;
+  }
+
+  .ant-upload-select-picture-card .ant-upload-text {
+    margin-top: 8px;
+    color: #666;
+  }
+`;
