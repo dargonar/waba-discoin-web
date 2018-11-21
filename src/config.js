7da45956dc42b43028bea10665e8465272f3c770
tuti <ptutino@gmail.com>
2018-11-21 00:44:10 -0300

    Versioned

--- src/config.js
+++ src/config.js
@@ -24,7 +24,7 @@ const versionConfig={
 const siteConfig = {
   siteName: "Discoin",
   siteIcon: "ion-flash",
-  footerText: "WABA.network ©2018 - v1.0.1",
+  footerText: "WABA.network ©2018 - v1.1.15",
   adminAccount: ["admin", "discoin-gov"] // (admin)-> PRIVATE TESNET || (discoin-dev) ->TUTI TESNET & MAIN NET
 };
 
