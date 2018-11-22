37239c8bf6c1b1dd23e823bcc35a580fe8be33a3
Marcos Gutierrez <gmarcos87@gmail.com>
2018-11-22 06:17:12 -0300

    Reduce duplicate code and unused functions

--- src/containers/Business/dashboard.js
+++ src/containers/Business/dashboard.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Children } from "react";
 import LayoutContentWrapper from "../../components/utility/layoutWrapper";
 import PageHeader from "../../components/utility/pageHeader";
 import IsoWidgetsWrapper from "../../components/utility/widgets-wrapper";
@@ -21,10 +21,18 @@ import {
   isConfiguration,
   todayDiscount,
   todayReward,
-  hasBalances,
   warnings,
-  rating
+  rating,
+  getBalances
 } from "../../redux/api/selectors/business.selectors";
+
+const { rowStyle, colStyle } = basicStyle;
+
+const ColWidget = ({ children }) => (
+  <Col md={6} sm={12} xs={24} style={colStyle}>
+    <IsoWidgetsWrapper>{children}</IsoWidgetsWrapper>
+  </Col>
+);
 
 export class Dashboard extends Component {
   constructor(props) {
@@ -34,7 +42,7 @@ export class Dashboard extends Component {
   }
 
   onPercentageClick() {
-    this.props.goTo("/dashboard/business/profile");
+    this.props.goTo("/dashboard/business/discount-and-rewards");
   }
 
   componentWillMount() {
@@ -43,96 +51,81 @@ export class Dashboard extends Component {
   }
 
   renderContent() {
-    const { rowStyle, colStyle } = basicStyle;
     return (
       <Row style={rowStyle} gutter={0} justify="start">
-        <Col md={6} sm={12} xs={24} style={colStyle}>
-          <IsoWidgetsWrapper>
-            <BalanceSticker
-              amount={this.props.balances.balance}
-              text={
-                <IntlMessage
-                  id="dashboard.balance"
-                  values={{
-                    currency: currency.symbol
-                  }}
-                  defaultMessage={"{currency} Balance"}
-                />
-              }
-              coin={currency.symbol}
-              fontColor="#1C222C"
-              bgColor="#fff"
-            />
-          </IsoWidgetsWrapper>
-        </Col>
+        <ColWidget>
+          <BalanceSticker
+            amount={this.props.balances.balance}
+            text={
+              <IntlMessage
+                id="dashboard.balance"
+                values={{
+                  currency: currency.symbol
+                }}
+                defaultMessage={"{currency} Balance"}
+              />
+            }
+            coin={currency.symbol}
+            fontColor="#1C222C"
+            bgColor="#fff"
+          />
+        </ColWidget>
 
-        <Col md={6} sm={12} xs={24} style={colStyle}>
-          <IsoWidgetsWrapper>
-            <BalanceSticker
-              amount={this.props.balances.initial_credit}
-              text={<IntlMessage defaultMessage="Initial Credit" id="dashboard.initialCredit" />}
-              coin={currency.symbol}
-              fontColor="#1C222C"
-              bgColor="#fff"
-            />
-          </IsoWidgetsWrapper>
-        </Col>
+        <ColWidget>
+          <BalanceSticker
+            amount={this.props.balances.initial_credit}
+            text={<IntlMessage defaultMessage="Initial Credit" id="dashboard.initialCredit" />}
+            coin={currency.symbol}
+            fontColor="#1C222C"
+            bgColor="#fff"
+          />
+        </ColWidget>
 
-        <Col md={6} sm={12} xs={24} style={colStyle}>
-          <IsoWidgetsWrapper>
-            <BalanceSticker
-              amount={this.props.balances.ready_to_access}
-              text={<IntlMessage defaultMessage="Endorsed" id="dashboard.endorsed" />}
-              coin={currency.symbol}
-              fontColor="#1C222C"
-              bgColor="#fff"
-            />
-            <OverdraftStrip />
-          </IsoWidgetsWrapper>
-        </Col>
+        <ColWidget>
+          <BalanceSticker
+            amount={this.props.balances.ready_to_access}
+            text={<IntlMessage defaultMessage="Endorsed" id="dashboard.endorsed" />}
+            coin={currency.symbol}
+            fontColor="#1C222C"
+            bgColor="#fff"
+          />
+          <OverdraftStrip />
+        </ColWidget>
 
-        <Col md={6} sm={12} xs={24} style={colStyle}>
-          <IsoWidgetsWrapper>
-            <BalanceSticker
-              amount={this.props.balanceRatio}
-              text={<IntlMessage defaultMessage="Accepted / Received ratio" id="dashboard.acceptedRatio" />}
-              scale={this.props.warnings}
-              percentage={true}
-              fontColor="#1C222C"
-              bgColor="#fff"
-            />
-          </IsoWidgetsWrapper>
-        </Col>
+        <ColWidget>
+          <BalanceSticker
+            amount={this.props.balanceRatio}
+            text={<IntlMessage defaultMessage="Accepted / Received ratio" id="dashboard.acceptedRatio" />}
+            scale={this.props.warnings}
+            percentage={true}
+            fontColor="#1C222C"
+            bgColor="#fff"
+          />
+        </ColWidget>
 
-        <Col md={6} sm={12} xs={24} style={colStyle} onClick={this.onPercentageClick}>
-          <IsoWidgetsWrapper>
-            <BalanceSticker
-              amount={this.props.todayDiscount}
-              percentage={true}
-              text={<IntlMessage defaultMessage="Reward & Discount" id="dashboard.todayDiscount" />}
-              fontColor="#1C222C"
-              bgColor="#fff"
-            />
-          </IsoWidgetsWrapper>
-        </Col>
+        <ColWidget onClick={this.onPercentageClick}>
+          <BalanceSticker
+            amount={this.props.todayDiscount}
+            percentage={true}
+            text={<IntlMessage defaultMessage="Reward & Discount" id="dashboard.todayDiscount" />}
+            fontColor="#1C222C"
+            bgColor="#fff"
+          />
+        </ColWidget>
 
-        <Col md={6} sm={12} xs={24} style={colStyle} onClick={this.onPercentageClick}>
-          <IsoWidgetsWrapper>
-            <BalanceSticker
-              amount={this.props.todayReward}
-              percentage={true}
-              text={<IntlMessage defaultMessage="Today reward %" id="dashboard.todayReward" />}
-              fontColor="#1C222C"
-              bgColor="#fff"
-            />
-          </IsoWidgetsWrapper>
-        </Col>
+        <ColWidget onClick={this.onPercentageClick}>
+          <BalanceSticker
+            amount={this.props.todayReward}
+            percentage={true}
+            text={<IntlMessage defaultMessage="Today reward %" id="dashboard.todayReward" />}
+            fontColor="#1C222C"
+            bgColor="#fff"
+          />
+        </ColWidget>
 
-        <Col md={6} sm={12} xs={24} style={colStyle}>
-          <IsoWidgetsWrapper>
-            <RatingSticker rating={this.props.rating} full={0} stars={0} text={0} icon="user" fontColor="#1C222C" bgColor="#fff" />
-          </IsoWidgetsWrapper>
-        </Col>
+        <ColWidget>
+          <RatingSticker rating={this.props.rating} full={0} stars={0} text={0} icon="user" fontColor="#1C222C" bgColor="#fff" />
+        </ColWidget>
       </Row>
     );
   }
@@ -151,11 +144,11 @@ export class Dashboard extends Component {
 }
 
 const mapStateToProps = state => ({
-  balanceRatio: balanceRatio(state),
   isReady: isBusiness(state) && isConfiguration(state) && !state.Api.loading,
   todayDiscount: todayDiscount(state),
   todayReward: todayReward(state),
-  balances: hasBalances(state) ? state.Api.business.balances : {},
+  balances: getBalances(state),
+  balanceRatio: balanceRatio(state),
   rating: rating(state),
   warnings: warnings(state)
 });
@@ -163,8 +156,6 @@ const mapStateToProps = state => ({
 const dispatchToProps = dispatch => ({
   fetchProfile: bindActionCreators(actions.fetchProfile, dispatch),
   fetchConfiguration: bindActionCreators(actions.fetchConfiguration, dispatch),
-  getSchedule: bindActionCreators(actions.getSchedule, dispatch),
-  cleanMsg: bindActionCreators(actions.cleanMsg, dispatch),
   goTo: url => dispatch(push(url))
 });
 
