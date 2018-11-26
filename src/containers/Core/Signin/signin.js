3a6b79a30aa4e6e13febcf93dc6f64383bed877d
Marcos Gutierrez <gmarcos87@gmail.com>
2018-11-26 11:58:23 -0300

    Show session modal after submitting the registration form

--- src/containers/Core/Signin/signin.js
+++ src/containers/Core/Signin/signin.js
@@ -17,10 +17,9 @@ import { siteConfig } from "../../../config";
 import IntlBox from "../LanguageSwitcher/withModal";
 
 import Image from "../../../image/logo.png";
-
-import { cleanMnemonics , getPath } from "../../../httpService";
+import { Modal } from "antd";
+import { cleanMnemonics, getPath } from "../../../httpService";
 import bip39 from "bip39";
-
 
 const { login, loginFromLocal, cleanStorage, register } = authAction;
 
@@ -39,11 +38,11 @@ class SignIn extends Component {
 
     this.sessionPasswordInput = React.createRef();
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleRegister = this.handleRegister.bind(this);
     this.loginLocal = this.loginLocal.bind(this);
     this.toggle = this.toggle.bind(this);
     this.cancelLocal = this.cancelLocal.bind(this);
     this._handleKeyPress = this._handleKeyPress.bind(this);
-
     this.state = {
       isIntlActivated: false
     };
@@ -82,11 +81,10 @@ class SignIn extends Component {
   }
 
   handleLogin = () => {
-    
     // let x = cleanMnemonics(bip39.generateMnemonic(null, null, bip39.wordlists.spanish));
     // alert (x);
     // return;
-    
+
     if (this.state.remember && this.state.rememberKey === "") {
       message.warning(this.props.intl.messages["core.sessionPasswordWarning"] || "You must enter a session PIN");
       return;
@@ -103,6 +101,42 @@ class SignIn extends Component {
     });
   };
 
+  handleRegister(data) {
+    let localPassword;
+    Modal.confirm({
+      title: "Contraseña de sesión",
+      content: (
+        <div>
+          <p>
+            {this.props.intl.messages["core.sessionPasswordInfo"] ||
+              `
+              You can save your account data in your browser for easy access. 
+              For them we need a session password. Please note that this password will never 
+              replace the one created during registration.
+              `}
+          </p>
+          <Input
+            onChange={e => (localPassword = e.target.value)}
+            placeholder={this.props.intl.messages["core.sessionPassword"] || "Session PIN"}
+          />
+        </div>
+      ),
+      onCancel: () => {
+        this.props.register(data);
+      },
+      onOk: () =>
+        new Promise((res, rej) => {
+          if (!localPassword || localPassword.length <= 0) {
+            message.warning(this.props.intl.messages["core.sessionPasswordWarning"] || "You must enter a session PIN");
+            rej();
+            return;
+          }
+          this.props.register(data, localPassword);
+          res();
+        })
+    });
+  }
+
   render() {
     const from = { pathname: `/dashboard/${this.props.userType}/` };
     if (this.props.isLoggedIn) {
@@ -118,7 +152,7 @@ class SignIn extends Component {
           cancel={() => {
             this.setState({ register: false });
           }}
-          submit={this.props.register}
+          submit={this.handleRegister}
           loading={this.props.loading}
           error={this.props.error}
         />
