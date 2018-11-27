2bba06f8775f401306a21d744fce39a010b06f22
Marcos Gutierrez <gmarcos87@gmail.com>
2018-11-27 11:36:07 -0300

    Add today totals in dashboard

--- src/containers/Business/dashboard.js
+++ src/containers/Business/dashboard.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import { BasicPage } from "../../components/basicPage";
+import PageHeader from "../../components/utility/pageHeader";
 import IsoWidgetsWrapper from "../../components/utility/widgets-wrapper";
 import { Col, Row } from "antd";
 import basicStyle from "../../config/basicStyle";
 import BalanceSticker from "../../components/balance-sticker/balance-sticker";
 import RatingSticker from "../../components/rating-sticker/rating-sticker";
-import IntlMessage from "../../components/utility/intlMessages";
+import IntlMessages from "../../components/utility/intlMessages";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import actions from "../../redux/api/actions";
@@ -24,7 +25,11 @@ import {
   getBalances
 } from "../../redux/api/selectors/business.selectors";
 
-const { rowStyle, colStyle } = basicStyle;
+import { txTodayTotals } from "../../redux/api/selectors/subAccounts.selectors";
+
+let { rowStyle, colStyle } = basicStyle;
+
+colStyle = { ...colStyle, marginBottom: "20px" };
 
 const ColWidget = ({ children }) => (
   <Col md={6} sm={12} xs={24} style={colStyle}>
@@ -32,6 +37,65 @@ const ColWidget = ({ children }) => (
   </Col>
 );
 
+const Box = props => (
+  <Col md={12} sm={24} style={colStyle}>
+    <IsoWidgetsWrapper>
+      <BalanceSticker {...props} />
+    </IsoWidgetsWrapper>
+  </Col>
+);
+
+const TransactionsStickers = ({ txs }) => (
+  <Col xs={24} md={24} style={colStyle}>
+    <Col xs={24} md={12} style={colStyle}>
+      <IsoWidgetsWrapper>
+        <PageHeader>
+          <IntlMessages defaultMessage="Discounts today" id="discounts.today" />
+        </PageHeader>
+      </IsoWidgetsWrapper>
+      <Box
+        amount={txs.discount.coin}
+        text={<IntlMessages defaultMessage="Discounts" id="discounts" />}
+        subtext={<IntlMessages defaultMessage="{currency} sent" id="discountsSent" values={{ currency: currency.plural }} />}
+        bgColor="#fff"
+        coin={currency.symbol}
+      />
+
+      <Box
+        amount={txs.discount.fiat}
+        text={<IntlMessages defaultMessage="Discounts" id="discounts" />}
+        subtext={<IntlMessages defaultMessage="Total invoiced" id="discounts.totalInvoiced" />}
+        bgColor="#fff"
+        coin={currency.fiat.symbol}
+      />
+    </Col>
+    <Col xs={24} md={12}>
+      <IsoWidgetsWrapper>
+        <PageHeader>
+          <IntlMessages defaultMessage="Refounds today" id="refund.refound.today" />
+        </PageHeader>
+      </IsoWidgetsWrapper>
+
+      <Box
+        amount={txs.refund.coin}
+        text={<IntlMessages defaultMessage="Refounds" id="refund.refound" />}
+        subtext={
+          <IntlMessages defaultMessage="Total accepted {currency}" id="refund.totalAccepted" values={{ currency: currency.plural }} />
+        }
+        bgColor="#fff"
+        coin={currency.symbol}
+      />
+
+      <Box
+        amount={txs.refund.fiat}
+        text={<IntlMessages defaultMessage="Refounds" id="refund.refound" />}
+        subtext={<IntlMessages defaultMessage="Total invoiced" id="discounts.totalInvoiced" />}
+        bgColor="#fff"
+        coin={currency.fiat.symbol}
+      />
+    </Col>
+  </Col>
+);
 export class Dashboard extends Component {
   constructor(props) {
     super(props);
@@ -46,6 +110,7 @@ export class Dashboard extends Component {
   componentWillMount() {
     this.props.fetchProfile();
     this.props.fetchConfiguration();
+    this.props.fetchTx();
   }
 
   renderContent() {
@@ -55,7 +120,7 @@ export class Dashboard extends Component {
           <BalanceSticker
             amount={this.props.balances.balance}
             text={
-              <IntlMessage
+              <IntlMessages
                 id="dashboard.balance"
                 values={{
                   currency: currency.symbol
@@ -72,7 +137,7 @@ export class Dashboard extends Component {
         <ColWidget>
           <BalanceSticker
             amount={this.props.balances.initial_credit}
-            text={<IntlMessage defaultMessage="Initial Credit" id="dashboard.initialCredit" />}
+            text={<IntlMessages defaultMessage="Initial Credit" id="dashboard.initialCredit" />}
             coin={currency.symbol}
             fontColor="#1C222C"
             bgColor="#fff"
@@ -82,7 +147,7 @@ export class Dashboard extends Component {
         <ColWidget>
           <BalanceSticker
             amount={this.props.balances.ready_to_access}
-            text={<IntlMessage defaultMessage="Endorsed" id="dashboard.endorsed" />}
+            text={<IntlMessages defaultMessage="Endorsed" id="dashboard.endorsed" />}
             coin={currency.symbol}
             fontColor="#1C222C"
             bgColor="#fff"
@@ -93,7 +158,7 @@ export class Dashboard extends Component {
         <ColWidget>
           <BalanceSticker
             amount={this.props.balanceRatio}
-            text={<IntlMessage defaultMessage="Accepted / Received ratio" id="dashboard.acceptedRatio" />}
+            text={<IntlMessages defaultMessage="Accepted / Received ratio" id="dashboard.acceptedRatio" />}
             scale={this.props.warnings}
             percentage={true}
             fontColor="#1C222C"
@@ -105,7 +170,7 @@ export class Dashboard extends Component {
           <BalanceSticker
             amount={this.props.todayDiscount}
             percentage={true}
-            text={<IntlMessage defaultMessage="Reward & Discount" id="dashboard.todayDiscount" />}
+            text={<IntlMessages defaultMessage="Reward & Discount" id="dashboard.todayDiscount" />}
             fontColor="#1C222C"
             bgColor="#fff"
           />
@@ -115,7 +180,7 @@ export class Dashboard extends Component {
           <BalanceSticker
             amount={this.props.todayReward}
             percentage={true}
-            text={<IntlMessage defaultMessage="Today reward %" id="dashboard.todayReward" />}
+            text={<IntlMessages defaultMessage="Today reward %" id="dashboard.todayReward" />}
             fontColor="#1C222C"
             bgColor="#fff"
           />
@@ -124,13 +189,14 @@ export class Dashboard extends Component {
         <ColWidget>
           <RatingSticker rating={this.props.rating} full={0} stars={0} text={0} icon="user" fontColor="#1C222C" bgColor="#fff" />
         </ColWidget>
+        <TransactionsStickers txs={this.props.txs} />
       </Row>
     );
   }
 
   render() {
     return (
-      <BasicPage title={<IntlMessage id="dashboard.dashboard" defaultMessage="Dashboard" />} loading={!this.props.isReady}>
+      <BasicPage title={<IntlMessages id="dashboard.dashboard" defaultMessage="Dashboard" />} loading={!this.props.isReady}>
         {this.props.isReady ? this.renderContent() : false}
       </BasicPage>
     );
@@ -144,12 +210,14 @@ const mapStateToProps = state => ({
   balances: getBalances(state),
   balanceRatio: balanceRatio(state),
   rating: rating(state),
-  warnings: warnings(state)
+  warnings: warnings(state),
+  txs: txTodayTotals(state.Api.transactions || [])
 });
 
 const dispatchToProps = dispatch => ({
   fetchProfile: bindActionCreators(actions.fetchProfile, dispatch),
   fetchConfiguration: bindActionCreators(actions.fetchConfiguration, dispatch),
+  fetchTx: bindActionCreators(actions.searchTransactions, dispatch),
   goTo: url => dispatch(push(url))
 });
 
